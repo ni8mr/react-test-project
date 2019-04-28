fd79baf3230c985fe949fc1d38c6fe0510f4d6c0
ni8mr <nilpaddo@gmail.com>
2019-04-28 13:04:00 +0600

    Split the mother component into reusable components.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -60,36 +60,60 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-          <form>
-              <input
-                  type="text"
-                  value={searchTerm}
-                  onChange={this.onSearchChange}
-              />
-          </form>
-          {list.filter(isSearched(searchTerm)).map(item =>
-              <div> key={item.objectID}
-                <span>
-                    <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-                <span>
-                    <button
-                        type='button'
-                        onClick={() => this.onDismiss(item.objectID)} // It has to be a function
-                        // Otherwise the class method would be executed immediately when application is opened at
-                        // the browser
-                    >
-                        Dismiss
-                    </button>
-                </span>
-              </div>
-          )}
+          <Search
+              value={searchTerm}
+              onChange={this.onSearchChange}
+          />
+          <Table list={list}
+              pattern={searchTerm}
+              onDismiss={this.onDismiss}
+          />
       </div>
     );
   }
+}
+
+class Search extends Component {
+    render () {
+        const { value, onChange } = this.props;
+        return (
+            <form>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                />
+            </form>
+        );
+    }
+}
+
+class Table extends Component {
+    render() {
+        const { list, pattern, onDismiss } = this.props;
+        return (
+            <div>
+                {list.filter(isSearched(pattern)).map(item =>
+                    <div key={item.objectID}>
+                        <span>
+                            <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                        <span>
+                            <button
+                                onClick={() => onDismiss(item.objectID)}
+                                type="button"
+                            >
+                                Dismiss
+                            </button>
+                        </span>
+                    </div>
+                )}
+            </div>
+        );
+    }
 }
 
 // if (module.hot) {
