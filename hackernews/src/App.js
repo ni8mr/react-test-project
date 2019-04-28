c861592dc3fcd52b13bd2e8f956e3ffcf0dc3958
ni8mr <nilpaddo@gmail.com>
2019-04-28 13:17:43 +0600

    Pass children props to a component.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -63,7 +63,9 @@ class App extends Component {
           <Search
               value={searchTerm}
               onChange={this.onSearchChange}
-          />
+          >
+              Search
+          </Search>
           <Table list={list}
               pattern={searchTerm}
               onDismiss={this.onDismiss}
@@ -75,10 +77,10 @@ class App extends Component {
 
 class Search extends Component {
     render () {
-        const { value, onChange } = this.props;
+        const { value, onChange, children } = this.props;
         return (
             <form>
-                <input
+                {children} <input
                     type="text"
                     value={value}
                     onChange={onChange}
